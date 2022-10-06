cc33c231c709714b20e005d1978fdd45aaa48e4c
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-10-06 20:06:00 +0100

    fix it 5

--- src/scripts/configure-git.sh
+++ src/scripts/configure-git.sh
@@ -3,7 +3,7 @@
 EMAIL=${!GIT_USER_EMAIL}
 USERNAME=${!GIT_USER_NAME}
 
-git config user.email "${EMAIL}"
+git config user.email "${!EMAIL}"
 git config user.name "${USERNAME}"
 
 git config user.email 
