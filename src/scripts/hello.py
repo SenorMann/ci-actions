d3d49a060738135af0fb62b64e67095209f1585b
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-10-06 20:31:45 +0100

    fix bug

--- src/scripts/hello.py
+++ src/scripts/hello.py
@@ -1,15 +1,15 @@
 import os
 import subprocess
 
-user_name_key = os.environ.get("GIT_USER_NAME")
-user_email_key = os.environ.get("GIT_USER_EMAIL")
+user_name_key = os.environ.get("PARAM_GIT_USER_NAME")
+user_email_key = os.environ.get("PARAM_GIT_USER_EMAIL")
 
 email = os.environ.get(user_email_key)
 name = os.environ.get(user_name_key)
 
-subprocess.run(f"git config user.name {name}")
-subprocess.run(f"git config user.email {email}")
+subprocess.run([f"git config user.name {name}"])
+subprocess.run([f"git config user.email {email}"])
 
 
-subprocess.run("git config user.name")
-subprocess.run("git config user.email")
+subprocess.run(["git config user.name"])
+subprocess.run(["git config user.email"])
