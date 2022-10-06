8d3639623df6232771d57bcd1f30c8683b0b6dc6
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-10-06 19:48:37 +0100

    fixit 2

--- src/scripts/configure-git.sh
+++ src/scripts/configure-git.sh
@@ -3,8 +3,8 @@
 EMAIL=$(eval echo \$${GIT_USER_EMAIL})
 USERNAME=$(eval echo \$${GIT_USER_NAME})
 
-git config user.email "$GIT_USER_EMAIL"
-git config user.name "$GIT_USER_NAME"
+git config user.email "$EMAIL"
+git config user.name "$USERNAME"
 
 git config user.email 
 git config user.name 
