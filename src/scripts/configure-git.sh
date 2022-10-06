a818819d94d35b696cb146318113fb5b799ccb2f
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-10-06 19:45:17 +0100

    fix it

--- src/scripts/configure-git.sh
+++ src/scripts/configure-git.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "$GIT_USER_EMAIL"
-GIT_USER_EMAIL=${!GIT_USER_EMAIL}
-echo "$GIT_USER_EMAIL"
+EMAIL=$(eval echo \$${GIT_USER_EMAIL})
+USERNAME=$(eval echo \$${GIT_USER_NAME})
+
 git config user.email "$GIT_USER_EMAIL"
 git config user.name "$GIT_USER_NAME"
+
+git config user.email 
+git config user.name 
